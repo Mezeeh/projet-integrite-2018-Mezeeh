ff7b09f382e82accf5e2068f323a2a6e833e1d6a
Unknown <1643216@cgmatane.qc.ca>
2018-09-05 21:09:54 -0400

    Classe de film de base (variable + getter et setter)

---
+++ Filmotheque/src/modele/Film.java
@@ -0,0 +1,51 @@
+package modele;
+
+public class Film {
+	protected String titre,
+					description,
+					genre,
+					dateDeSortie,
+					duree;
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getDateDeSortie() {
+		return dateDeSortie;
+	}
+
+	public void setDateDeSortie(String dateDeSortie) {
+		this.dateDeSortie = dateDeSortie;
+	}
+
+	public String getDuree() {
+		return duree;
+	}
+
+	public void setDuree(String duree) {
+		this.duree = duree;
+	}
+	
+	
+}
