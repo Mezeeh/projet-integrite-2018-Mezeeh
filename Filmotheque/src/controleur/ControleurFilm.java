b893d6cf65f21e2c352bca32145b2b8cc3f738dd
Unknown <1643216@cgmatane.qc.ca>
2018-09-05 22:50:55 -0400

    Test dans controleur plutot que du navigateur des vues

--- Filmotheque/src/controleur/ControleurFilm.java
+++ Filmotheque/src/controleur/ControleurFilm.java
@@ -1,18 +1,65 @@
 package controleur;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Film;
 import vue.NavigateurDesVues;
+import vue.VueAjouterFilm;
+import vue.VueFilm;
+import vue.VueListeFilm;
 
 public class ControleurFilm {
 
 	private NavigateurDesVues navigateur;
 	
+	private VueAjouterFilm vueAjouterFilm = null;
+	private VueListeFilm vueListeFilm = null;
+	private VueFilm vueFilm = null;
+	
 	public ControleurFilm(NavigateurDesVues navigateur)
 	{
 		this.navigateur = navigateur;
+		this.vueAjouterFilm = navigateur.getVueAjouterFilm();
+		this.vueListeFilm = navigateur.getVueListeFilm();
+		this.vueFilm = navigateur.getVueFilm();
+		
 		System.out.println("Initialisation du controleur");
 		
-		this.navigateur.naviguerVersVueListeFilm();
+		//// TEST ////
+		Film film = new Film("Die Hard",
+				"Un policier new-yorkais, John McClane, est séparé de sa femme Holly, cadre dans une puissante multinationale japonaise, la Nakatomi Corporation. Venu à Los Angeles passer les fêtes avec elle, il se rend à la tour Nakatomi où le patron donne une grande soirée. Tandis que John s'isole pour téléphoner, un groupe de terroristes allemands, dirigé par Hans Gruber, pénètre dans l'immeuble.",
+				"énigme/Thriller",
+				"1988",
+				"2h 12m");
+		this.vueFilm.afficherFilm(film);
 		this.navigateur.naviguerVersVueFilm();
-		this.navigateur.naviguerVersVueAjouterFilm();
+		
+		/// TEST ///
+		List<Film> listeFilmsTest = new ArrayList<Film>();
+		listeFilmsTest.add(new Film("Die Hard",
+				"Un policier new-yorkais, John McClane, est séparé de sa femme Holly, cadre dans une puissante multinationale japonaise, la Nakatomi Corporation. Venu à Los Angeles passer les fêtes avec elle, il se rend à la tour Nakatomi où le patron donne une grande soirée. Tandis que John s'isole pour téléphoner, un groupe de terroristes allemands, dirigé par Hans Gruber, pénètre dans l'immeuble.",
+				"énigme/Thriller",
+				"1988",
+				"2h 12m"));
+		listeFilmsTest.add(new Film("Rambo",
+				"Revenu du Viêtnam, abruti autant par les mauvais traitements que lui ont jadis infligés ses tortionnaires que par l'indifférence de ses concitoyens, le soldat Rambo, un ancien des commandos d'élite, traîne sa redoutable carcasse de ville en ville. Un shérif teigneux lui interdit l'accès de sa bourgade. Rambo insiste. Il veut seulement manger. Le shérif le met sous les verrous et laisse son adjoint brutaliser ce divertissant clochard.",
+				"Drame/Thriller",
+				"1982",
+				"1h 33m"));
+		listeFilmsTest.add(new Film("Rocky",
+				"Rocky Balboa travaille pour Tony Gazzo, un usurier, et dispute de temps à autre des combats de boxe pour quelques dizaines de dollars sous l'appellation de l'Étalon Italien. Cependant, Mickey, propriétaire du club de boxe où Rocky a l'habitude de s'entraîner, décide de céder son casier à un boxeur plus talentueux.",
+				"Drame/Sport",
+				"1976",
+				"2h 2m"));
+		listeFilmsTest.add(new Film("Le Seigneur des anneaux : La Communauté de l'anneau",
+				"Un jeune et timide `Hobbit', Frodon Sacquet, hérite d'un anneau magique. Bien loin d'être une simple babiole, il s'agit d'un instrument de pouvoir absolu qui permettrait à Sauron, le `Seigneur des ténèbres', de régner sur la `Terre du Milieu' et de réduire en esclavage ses peuples. Frodon doit parvenir jusqu'à la `Crevasse du Destin' pour détruire l'anneau.",
+				"fantasy/Action",
+				"2001",
+				"3h 48m"));
+		this.vueListeFilm.afficherListeFilms(listeFilmsTest);
+		this.navigateur.naviguerVersVueListeFilm();
+		
+		//this.navigateur.naviguerVersVueAjouterFilm();
 	}
 }
--- Filmotheque/src/vue/NavigateurDesVues.java
+++ Filmotheque/src/vue/NavigateurDesVues.java
@@ -23,38 +23,6 @@ public class NavigateurDesVues extends Application{
 		this.vueAjouterFilm = new VueAjouterFilm();
 		this.vueListeFilm = new VueListeFilm();
 		this.vueFilm = new VueFilm();
-		
-		/// TEST ///
-		List<Film> listeFilmsTest = new ArrayList<Film>();
-		listeFilmsTest.add(new Film("Die Hard",
-				"Un policier new-yorkais, John McClane, est séparé de sa femme Holly, cadre dans une puissante multinationale japonaise, la Nakatomi Corporation. Venu à Los Angeles passer les fêtes avec elle, il se rend à la tour Nakatomi où le patron donne une grande soirée. Tandis que John s'isole pour téléphoner, un groupe de terroristes allemands, dirigé par Hans Gruber, pénètre dans l'immeuble.",
-				"énigme/Thriller",
-				"1988",
-				"2h 12m"));
-		listeFilmsTest.add(new Film("Rambo",
-				"Revenu du Viêtnam, abruti autant par les mauvais traitements que lui ont jadis infligés ses tortionnaires que par l'indifférence de ses concitoyens, le soldat Rambo, un ancien des commandos d'élite, traîne sa redoutable carcasse de ville en ville. Un shérif teigneux lui interdit l'accès de sa bourgade. Rambo insiste. Il veut seulement manger. Le shérif le met sous les verrous et laisse son adjoint brutaliser ce divertissant clochard.",
-				"Drame/Thriller",
-				"1982",
-				"1h 33m"));
-		listeFilmsTest.add(new Film("Rocky",
-				"Rocky Balboa travaille pour Tony Gazzo, un usurier, et dispute de temps à autre des combats de boxe pour quelques dizaines de dollars sous l'appellation de l'Étalon Italien. Cependant, Mickey, propriétaire du club de boxe où Rocky a l'habitude de s'entraîner, décide de céder son casier à un boxeur plus talentueux.",
-				"Drame/Sport",
-				"1976",
-				"2h 2m"));
-		listeFilmsTest.add(new Film("Le Seigneur des anneaux : La Communauté de l'anneau",
-				"Un jeune et timide `Hobbit', Frodon Sacquet, hérite d'un anneau magique. Bien loin d'être une simple babiole, il s'agit d'un instrument de pouvoir absolu qui permettrait à Sauron, le `Seigneur des ténèbres', de régner sur la `Terre du Milieu' et de réduire en esclavage ses peuples. Frodon doit parvenir jusqu'à la `Crevasse du Destin' pour détruire l'anneau.",
-				"fantasy/Action",
-				"2001",
-				"3h 48m"));
-		this.vueListeFilm.afficherListeFilms(listeFilmsTest); // Appel de ma fonction avant de la programmer (pour tester à mesure)
-		
-		//// TEST ////
-		Film film = new Film("Die Hard",
-				"Un policier new-yorkais, John McClane, est séparé de sa femme Holly, cadre dans une puissante multinationale japonaise, la Nakatomi Corporation. Venu à Los Angeles passer les fêtes avec elle, il se rend à la tour Nakatomi où le patron donne une grande soirée. Tandis que John s'isole pour téléphoner, un groupe de terroristes allemands, dirigé par Hans Gruber, pénètre dans l'immeuble.",
-				"énigme/Thriller",
-				"1988",
-				"2h 12m");
-		this.vueFilm.afficherFilm(film);; // Appel de ma fonction avant de la programmer (pour tester à mesure)
 	}
 	
 	@Override
