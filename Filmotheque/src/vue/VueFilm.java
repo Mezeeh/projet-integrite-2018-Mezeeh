f5a14106425b85e3dfe01ff303ba4cbf42c5105d
Unknown <1643216@cgmatane.qc.ca>
2018-09-05 21:25:00 -0400

    Affichage des donnees avec le setText des labels

--- Filmotheque/src/vue/VueFilm.java
+++ Filmotheque/src/vue/VueFilm.java
@@ -25,23 +25,23 @@ public class VueFilm extends Application{
 		panneau = new Pane();
 		grilleFilm = new GridPane();
 		
-		valeurTitre = new Label("Die Hard");
+		valeurTitre = new Label();
 		grilleFilm.add(new Label("Titre : "), 0, 0);
 		grilleFilm.add(valeurTitre, 1, 0);
 		
-		valeurDescription = new Label("Un policier new-yorkais, John McClane, est séparé de sa femme Holly, cadre dans une puissante multinationale japonaise, la Nakatomi Corporation. Venu à Los Angeles passer les fêtes avec elle, il se rend à la tour Nakatomi où le patron donne une grande soirée. Tandis que John s'isole pour téléphoner, un groupe de terroristes allemands, dirigé par Hans Gruber, pénètre dans l'immeuble.");
+		valeurDescription = new Label();
 		grilleFilm.add(new Label("Description : "), 0, 1);
 		grilleFilm.add(valeurDescription, 1, 1);
 		
-		valeurGenre = new Label("énigme/Thriller");
+		valeurGenre = new Label();
 		grilleFilm.add(new Label("Genre : "), 0, 2);
 		grilleFilm.add(valeurGenre, 1, 2);
 		
-		valeurDateDeSortie = new Label("1988");
+		valeurDateDeSortie = new Label();
 		grilleFilm.add(new Label("Date de sortie : "), 0, 3);
 		grilleFilm.add(valeurDateDeSortie, 1, 3);
 		
-		valeurDuree = new Label("2h 12m");
+		valeurDuree = new Label();
 		grilleFilm.add(new Label("Duree : "), 0, 4);
 		grilleFilm.add(valeurDuree, 1, 4);
 		
@@ -61,6 +61,10 @@ public class VueFilm extends Application{
 	}
 
 	public void afficherFilm(Film film) {
-		
+		this.valeurTitre.setText(film.getTitre());
+		this.valeurDescription.setText(film.getDescription());
+		this.valeurGenre.setText(film.getGenre());
+		this.valeurDateDeSortie.setText(film.getDateDeSortie());
+		this.valeurDuree.setText(film.getDuree());
 	}
 }
