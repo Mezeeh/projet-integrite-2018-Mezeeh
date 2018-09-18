eaef6bb652abb4077d0d3d773cd6970feddd4bd9
Unknown <1643216@cgmatane.qc.ca>
2018-09-17 21:32:43 -0400

    Classe pour centraliser les informations de connexion avec la base de donnes

---
+++ Filmotheque/src/accesseur/Acces.java
@@ -0,0 +1,8 @@
+package accesseur;
+
+public class Acces {
+	public static String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
+	public static String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/filmotheque";
+	public static String BASEDEDONNEES_USAGER = "postgres";
+	public static String BASEDEDONNEES_MOTDEPASSE = "sudoroot";
+}
