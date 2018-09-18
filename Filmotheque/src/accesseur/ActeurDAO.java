a9930855e3b7222bc245a4017eb6e13c11f574f4
Unknown <1643216@cgmatane.qc.ca>
2018-09-17 21:29:03 -0400

    Classe pour centraliser la connexion a la base de donnes

--- Filmotheque/src/accesseur/ActeurDAO.java
+++ Filmotheque/src/accesseur/ActeurDAO.java
@@ -1,7 +1,6 @@
 package accesseur;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,26 +10,10 @@ import java.util.List;
 import modele.Acteur;
 
 public class ActeurDAO {
-
-	private static String BASEDEDONNEES_DRIVER = "org.postgresql.Driver";
-	private static String BASEDEDONNEES_URL = "jdbc:postgresql://localhost:5432/filmotheque";
-	private static String BASEDEDONNEES_USAGER = "postgres";
-	private static String BASEDEDONNEES_MOTDEPASSE = "sudoroot";
-	
 	private Connection connection;
 	
 	public ActeurDAO() {
-		try {
-			Class.forName(BASEDEDONNEES_DRIVER);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			connection = DriverManager.getConnection(BASEDEDONNEES_URL, BASEDEDONNEES_USAGER, BASEDEDONNEES_MOTDEPASSE);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		connection = BaseDeDonnees.getInstance().getConnection();
 	}
 	
 	public List<Acteur> listerActeurs(int idFilm){
