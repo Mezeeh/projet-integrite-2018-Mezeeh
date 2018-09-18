a06cd245d01c4eafef3477956ca188b9fa5731ae
Unknown <1643216@cgmatane.qc.ca>
2018-09-17 22:14:12 -0400

    Requetes SQL dans les interfaces en public static final (constante)

--- Filmotheque/src/accesseur/ActeurSQL.java
+++ Filmotheque/src/accesseur/ActeurSQL.java
@@ -2,6 +2,6 @@ package accesseur;
 
 public interface ActeurSQL {
 
-	String SQL_LISTER_ACTEUR_PAR_FILM = "SELECT * FROM acteur WHERE id_film = ?;";
+	public static final String SQL_LISTER_ACTEUR_PAR_FILM = "SELECT * FROM acteur WHERE id_film = ?;";
 	
 }
--- Filmotheque/src/accesseur/FilmSQL.java
+++ Filmotheque/src/accesseur/FilmSQL.java
@@ -2,9 +2,9 @@ package accesseur;
 
 public interface FilmSQL {
 
-	String SQL_LISTER_FILM = "SELECT * FROM film;";
-	String SQL_AJOUTER_FILM = "INSERT INTO film(titre, description, genre, date_de_sortie, duree) VALUES(?, ?, ?, ?, ?);";
-	String SQL_MODIFIER_FILM = "UPDATE film SET titre = ?, description = ?, genre = ?, date_de_sortie = ?, duree = ? WHERE id = ?;";
-	String SQL_RAPPORTER_FILM = "SELECT * FROM film WHERE id = ?";
+	public static final String SQL_LISTER_FILM = "SELECT * FROM film;";
+	public static final String SQL_AJOUTER_FILM = "INSERT INTO film(titre, description, genre, date_de_sortie, duree) VALUES(?, ?, ?, ?, ?);";
+	public static final String SQL_MODIFIER_FILM = "UPDATE film SET titre = ?, description = ?, genre = ?, date_de_sortie = ?, duree = ? WHERE id = ?;";
+	public static final String SQL_RAPPORTER_FILM = "SELECT * FROM film WHERE id = ?";
 	
 }
